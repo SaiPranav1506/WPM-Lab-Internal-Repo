18a6058568af857c4f0e6ed53b36b29d5536b7c4
SaiPranav1506 <saipranavkanduri@gmail.com>
2025-08-21 10:48:30 +0530

    First Commit

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -6,15 +6,3 @@ module.exports={
 };
 
 
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-var ctrlLocations=require('../controllers/locations');
-var ctrlOthers=require('../controllers/others');
-router.get('/',ctrlLocations.homelist);
-router.get('/location',ctrlLocations.locationInfo);
-router.get('/location/review/new',ctrlLocations.addReview);
-router.get('/about',ctrlOthers.about);
-module.exports=router;
-
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,12 @@
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pranav 23EG107E22' });
-});
-
-module.exports = router;
+var ctrlLocations=require('../controllers/locations');
+var ctrlOthers=require('../controllers/others');
+router.get('/',ctrlLocations.homelist);
+router.get('/location',ctrlLocations.locationInfo);
+router.get('/location/review/new',ctrlLocations.addReview);
+router.get('/about',ctrlOthers.about);
+module.exports=router;
